44b400fc2f3324ec86f6a627202e23cc9d333a7e
kleefd <denny.van.kleef@rabobank.nl>
2019-09-11 16:14:18 +0200

    Created objects for response. Added test

---
+++ WeatherFunction.Tests/CurrentWeatherTests.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace WeatherFunction.Tests
+{
+    public class CurrentWeatherTests
+    {
+        [Fact]
+        public async Task Test()
+        {
+            //Arrange
+            var request = new Mock<HttpRequest>();
+            var logger = new Mock<ILogger>();
+
+            //Act
+            var function = new CurrentWeather();
+            var rootObject = await function.Run(request.Object, logger.Object);
+
+            //Assert
+            Assert.NotNull(rootObject);
+        }
+    }
+}
